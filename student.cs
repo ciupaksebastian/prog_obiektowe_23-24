d5fdfb957ba8a29d5c9f3416deaf8511be0ec0ab
ciupaksebastian <166227481+ciupaksebastian@users.noreply.github.com>
2024-11-17 11:18:19 +0100

    lab2

---
+++ student.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Student: Osoba
+    {
+        private string NrAlbumu { get; set; }
+
+        public Student(string firstName, string lastName, int age, string nrAlbumu):
+            base(firstName,lastName,age)
+        {
+            NrAlbumu = nrAlbumu;
+        }
+        public void ViewStudent()
+        {
+            View();
+            Console.WriteLine($"NR albumu: {NrAlbumu}");
+        }
+    }
+}
